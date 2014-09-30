a5c6b796fc2c6ebd82dba400f4f66bdf739a6bda
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2014-09-30 18:19:16 +0200

    EclipseFS refactoring.

--- src/action/maintenancemilestoneaction.py
+++ src/action/maintenancemilestoneaction.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2.6
-# vi: sw=4 ts=4 expandtab smarttab ai smartindent
 from action.tasksaction import TasksAction
 from eclipsefs import EclipseFS
 from tasks.publishrepository import PublishRepository
@@ -7,24 +6,24 @@ from tasks.copyproductsfolder import CopyProductsFolder
 
 class MaintenanceMilestoneAction(TasksAction):
     def gettasks(self,args):
-        
+
         efs = EclipseFS(args.directory)
         milestone_version = args.milestoneversion
         project_name = "ldt"
         product_name = "ldt"
         remote_name = "remote"
-        
+
         return[PublishRepository("Publish new Maintenance Milestones Release Repositories",
-                                        efs.nightly_maintenance_repository(""),
-                                        efs.release_milestones_composite_repository(""),
+                                        efs.nightly_maintenance_repository(),
+                                        efs.release_milestones_composite_repository(),
                                         milestone_version,
                                         efs.stats_uri(project_name),
                                         [efs.feature_id(product_name),efs.feature_id(remote_name)]
                                         ),
                CopyProductsFolder("Deliver new Maintenance Milestones Products",
-                                        efs.nightly_maintenance_products_directory(""),
+                                        efs.nightly_maintenance_products_directory(),
                                         efs.release_milestones_products_directory("", milestone_version),
                                         efs.products_filenames(product_name))]
-        
+
     def name(self):
         return "Maintenance Milestone Action"
--- src/eclipsefs.py
+++ src/eclipsefs.py
@@ -1,4 +1,3 @@
-# vi: sw=4 ts=4 expandtab smarttab ai smartindent
 import os
 
 class EclipseFS:
@@ -12,124 +11,149 @@ class EclipseFS:
                                'org.eclipse.ldt.product-win32.win32.x86.zip',
                                'org.eclipse.ldt.product-linux.gtk.x86.tar.gz',
                                'org.eclipse.ldt.product-win32.win32.x86_64.zip']}
-    
+
     _STATS_URI = {'ldt':'http://download.eclipse.org/stats/ldt'}
-    
+
     _FEATURE_ID = {'ldt':'org.eclipse.ldt',
 		   'remote': 'org.eclipse.ldt.remote'}
-    
+
     def __init__(self, root=None):
-        # root is used for test only      
+        # root is used for test only
         if not root:
-            root='/' 
-        
+            root='/'
+
         # Download constant
-        self.DOWNLOAD_ROOT_DIRECTORY = os.path.join(root,'home/data/httpd/download.eclipse.org/ldt/')
-    
-        self.RELEASE_ROOT_PRODUCTS_DIRECTORY = self.DOWNLOAD_ROOT_DIRECTORY + 'products/'
-        self.RELEASE_STABLE_PRODUCTS_DIRECTORY = self.RELEASE_ROOT_PRODUCTS_DIRECTORY + 'stable/'
-        self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY = self.RELEASE_ROOT_PRODUCTS_DIRECTORY + 'milestones/'
-    
-        self.RELEASE_ROOT_REPOSITORY = self.DOWNLOAD_ROOT_DIRECTORY + 'releases/'
-        self.RELEASE_STABLE_REPOSITORY = self.RELEASE_ROOT_REPOSITORY + 'stable/'
-        self.RELEASE_MILESTONES_REPOSITORY = self.RELEASE_ROOT_REPOSITORY + 'milestones/'
-        
-        self.NIGHTLY_REPOSITORY = self.DOWNLOAD_ROOT_DIRECTORY + "updates-nightly/"
-        self.NIGHTLY_MAINTENANCE_REPOSITORY = self.DOWNLOAD_ROOT_DIRECTORY + "updates-nightly-maintenance/"
-        
+        self.DOWNLOAD_ROOT_DIRECTORY = os.path.join(root,
+            'home/data/httpd/download.eclipse.org/ldt')
+
+        self.RELEASE_ROOT_PRODUCTS_DIRECTORY = os.path.join(
+            self.DOWNLOAD_ROOT_DIRECTORY, 'products')
+        self.RELEASE_STABLE_PRODUCTS_DIRECTORY = os.path.join(
+            self.RELEASE_ROOT_PRODUCTS_DIRECTORY, 'stable')
+        self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY = os.path.join(
+            self.RELEASE_ROOT_PRODUCTS_DIRECTORY, 'milestones')
+
+        self.RELEASE_ROOT_REPOSITORY = os.path.join(
+            self.DOWNLOAD_ROOT_DIRECTORY, 'releases')
+        self.RELEASE_STABLE_REPOSITORY = os.path.join(
+            self.RELEASE_ROOT_REPOSITORY, 'stable')
+        self.RELEASE_MILESTONES_REPOSITORY = os.path.join(
+            self.RELEASE_ROOT_REPOSITORY, 'milestones')
+
+        self.NIGHTLY_REPOSITORY = os.path.join(self.DOWNLOAD_ROOT_DIRECTORY,
+            'updates-nightly')
+        self.NIGHTLY_MAINTENANCE_REPOSITORY = os.path.join(
+            self.DOWNLOAD_ROOT_DIRECTORY, 'updates-nightly-maintenance')
+
         # strange case for products we search the nightly on continuous integration
-        self.SHARED_LDT_FOLDER = os.path.join(root,"shared/tools/ldt/")
-        self.SHARED_PRODUCTS_FOLDER = self.SHARED_LDT_FOLDER + "products/"
-        self.NIGHTLY_PRODUCTS_DIRECTORY = self.SHARED_PRODUCTS_FOLDER + "nightly"
-        self.NIGHTLY_MAINTENANCE_PRODUCTS_DIRECTORY = self.SHARED_PRODUCTS_FOLDER + "nightly-maintenance"
-        
+        self.SHARED_LDT_FOLDER = os.path.join(root, 'shared/tools/ldt')
+        self.SHARED_PRODUCTS_FOLDER = os.path.join(self.SHARED_LDT_FOLDER,
+            'products')
+        self.NIGHTLY_PRODUCTS_DIRECTORY = os.path.join(
+            self.SHARED_PRODUCTS_FOLDER, 'nightly')
+        self.NIGHTLY_MAINTENANCE_PRODUCTS_DIRECTORY = os.path.join(
+            self.SHARED_PRODUCTS_FOLDER, 'nightly-maintenance')
+
         # Archive constant
-        self.ARCHIVE_ROOT_DIRECTORY = os.path.join(root,'home/data/httpd/archive.eclipse.org/ldt/')
-        
-        self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY = self.ARCHIVE_ROOT_DIRECTORY + 'products/'
-        self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY = self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY + 'milestones/'
-        self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY = self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY + 'stable/'
-        
-        self.ARCHIVE_ROOT_REPOSITORY = self.ARCHIVE_ROOT_DIRECTORY + 'releases/'
-        self.ARCHIVE_STABLE_REPOSITORY = self.ARCHIVE_ROOT_REPOSITORY + 'stable/'
-        self.ARCHIVE_MILESTONES_REPOSITORY = self.ARCHIVE_ROOT_REPOSITORY + 'milestones/'
-    
+        self.ARCHIVE_ROOT_DIRECTORY = os.path.join(root,
+            'home/data/httpd/archive.eclipse.org/ldt')
+
+        self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY = os.path.join(
+            self.ARCHIVE_ROOT_DIRECTORY, 'products')
+        self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY = os.path.join(
+            self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY, 'milestones')
+        self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY = os.path.join(
+            self.ARCHIVE_ROOT_PRODUCTS_DIRECTORY, 'stable')
+
+        self.ARCHIVE_ROOT_REPOSITORY = os.path.join(self.ARCHIVE_ROOT_DIRECTORY,
+            'releases')
+        self.ARCHIVE_STABLE_REPOSITORY = os.path.join(
+            self.ARCHIVE_ROOT_REPOSITORY, 'stable')
+        self.ARCHIVE_MILESTONES_REPOSITORY = os.path.join(
+            self.ARCHIVE_ROOT_REPOSITORY, 'milestones')
+
     # products name
     ############################
-    def products_filenames(self,product):
+    def products_filenames(self, product):
         return self._PRODUCT_FILENAMES[product]
-    
+
     # statistics
     ############################
-    def stats_uri(self,product):
+    def stats_uri(self, product):
         return self._STATS_URI[product]
-    
-    def feature_id(self,product):
+
+    def feature_id(self, product):
         return self._FEATURE_ID[product]
-        
+
     # Release paths
     ############################
-    def release_stable_composite_repository(self, product):
+    def release_stable_composite_repository(self, product=''):
         return os.path.join(self.RELEASE_STABLE_REPOSITORY, product)
-    
-    def release_stable_repository(self, product, version):
-        return os.path.join(self.RELEASE_STABLE_REPOSITORY, product,version)
-    
-    def release_stable_allversion_products_directory(self,product):
-        return os.path.join(self.RELEASE_STABLE_PRODUCTS_DIRECTORY,product)
 
-    def release_stable_products_directory(self,product,version):
-        return os.path.join(self.RELEASE_STABLE_PRODUCTS_DIRECTORY,product,version)
-    
-    def release_milestones_composite_repository(self, product):
+    def release_stable_repository(self, product='', version=''):
+        return os.path.join(self.RELEASE_STABLE_REPOSITORY, product, version)
+
+    def release_stable_allversion_products_directory(self, product=''):
+        return os.path.join(self.RELEASE_STABLE_PRODUCTS_DIRECTORY, product)
+
+    def release_stable_products_directory(self, product='', version=''):
+        return os.path.join(self.RELEASE_STABLE_PRODUCTS_DIRECTORY, product,
+            version)
+
+    def release_milestones_composite_repository(self, product=''):
         return os.path.join(self.RELEASE_MILESTONES_REPOSITORY, product)
-    
-    def release_milestones_repository(self, product, version):
-        return os.path.join(self.RELEASE_MILESTONES_REPOSITORY, product,version)
-        
-    def release_milestones_allversion_products_directory(self,product):
-        return os.path.join(self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY,product)
 
-    def release_milestones_products_directory(self,product,version):
-        return os.path.join(self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY,product,version)
-    
+    def release_milestones_repository(self, product='', version=''):
+        return os.path.join(self.RELEASE_MILESTONES_REPOSITORY, product,
+            version)
+
+    def release_milestones_allversion_products_directory(self,product=''):
+        return os.path.join(self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY, product)
+
+    def release_milestones_products_directory(self,product='', version=''):
+        return os.path.join(self.RELEASE_MILESTONES_PRODUCTS_DIRECTORY, product,
+            version)
+
     # Archive paths
     ############################
-    def archive_stable_composite_repository(self, product):
+    def archive_stable_composite_repository(self, product=''):
         return os.path.join(self.ARCHIVE_STABLE_REPOSITORY, product)
-    
-    def archive_stable_repository(self, product, version):
-        return os.path.join(self.ARCHIVE_STABLE_REPOSITORY, product,version)
-    
-    def archive_stable_allversion_products_directory(self,product):
-        return os.path.join(self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY,product)
 
-    def archive_stable_products_directory(self,product,version):
-        return os.path.join(self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY,product,version)
-    
-    def archive_milestones_composite_repository(self, product):
+    def archive_stable_repository(self, product='', version=''):
+        return os.path.join(self.ARCHIVE_STABLE_REPOSITORY, product, version)
+
+    def archive_stable_allversion_products_directory(self,product=''):
+        return os.path.join(self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY, product)
+
+    def archive_stable_products_directory(self, product='', version=''):
+        return os.path.join(self.ARCHIVE_STABLE_PRODUCTS_DIRECTORY, product,
+            version)
+
+    def archive_milestones_composite_repository(self, product=''):
         return os.path.join(self.ARCHIVE_MILESTONES_REPOSITORY, product)
-    
-    def archive_milestones_repository(self, product, version):
-        return os.path.join(self.ARCHIVE_MILESTONES_REPOSITORY, product,version)
 
-    def archive_milestones_allversion_products_directory(self,product):
-        return os.path.join(self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY,product)
+    def archive_milestones_repository(self, product='', version=''):
+        return os.path.join(self.ARCHIVE_MILESTONES_REPOSITORY, product,
+            version)
 
-    def archive_milestones_products_directory(self,product,version):
-        return os.path.join(self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY,product,version)
-    
+    def archive_milestones_allversion_products_directory(self, product=''):
+        return os.path.join(self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY, product)
+
+    def archive_milestones_products_directory(self, product='', version=''):
+        return os.path.join(self.ARCHIVE_MILESTONES_PRODUCTS_DIRECTORY, product,
+            version)
+
     # Nightly paths
     ############################
-    def nightly_repository(self,product):
-        return os.path.join(self.NIGHTLY_REPOSITORY,product)
-    
-    def nightly_products_directory(self,product):
-        return self.NIGHTLY_PRODUCTS_DIRECTORY.format(product)
-    
-    def nightly_maintenance_products_directory(self,product):
-        return self.NIGHTLY_MAINTENANCE_PRODUCTS_DIRECTORY.format(product)
-    
-    def nightly_maintenance_repository(self,product):
-        return os.path.join(self.NIGHTLY_MAINTENANCE_REPOSITORY,product)
-    
+    def nightly_repository(self, product=''):
+        return os.path.join(self.NIGHTLY_REPOSITORY, product)
+
+    def nightly_products_directory(self, product=''):
+        return os.path.join(self.NIGHTLY_PRODUCTS_DIRECTORY, product)
+
+    def nightly_maintenance_products_directory(self, product=''):
+        return os.path.join(self.NIGHTLY_MAINTENANCE_PRODUCTS_DIRECTORY,
+            product)
+
+    def nightly_maintenance_repository(self, product=''):
+        return os.path.join(self.NIGHTLY_MAINTENANCE_REPOSITORY, product)
